644c24e2dd7900a9f31cbf58a88b77b2aa4e9fdb
khuattiendat <khuattiendat1@gmail.com>
2024-08-15 17:16:40 +0700

    commit

--- server/src/routes/questionRouter.js
+++ server/src/routes/questionRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const questionController = require('../controllers/questionController');
+const questionController = require('../controllers/QuestionController');
 
 router.post('/create', questionController.addQuestion);
 
